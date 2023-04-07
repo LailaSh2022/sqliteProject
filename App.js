db8cdfe91a3582729dee1ab9b700e6708c379196
Laila Shihada <764700695@nzse.ac.nz>
2023-04-08 00:24:15 +1200

    Create SQLite local DB

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, useEffect } from "react-native";
+import {openDatabase} from "expo-sqlite";
+import React, { useState } from 'react';
 
-export default function App() {
+const db = openDatabase("surfsAppDB.db");
+console.log('db:', db);
+// Create the items table if it doesn't exist
+db.transaction((tx) => {
+  tx.executeSql(
+    "CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)"
+    );
+    tx.executeSql('INSERT INTO items (name) VALUES (?)', ['Item 1']);
+    tx.executeSql('INSERT INTO items (name) VALUES (?)', ['Item 2']);
+    tx.executeSql('INSERT INTO items (name) VALUES (?)', ['Item 3']);
+  });
+
+const App = () => {
+  // State to store the name of the selected item
+  const [selectedItemName, setSelectedItemName] = React.useState("");
+
+  // Function to select the name of an item from the database
+  const selectItemName = () => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "SELECT name FROM items WHERE id = ?",
+        [1], // Replace 1 with the ID of the item you want to select
+        (_, { rows }) => setSelectedItemName(rows.item(0).name),
+        (_, error) => console.error(error)
+      );
+    });
+  };
+
+  // Call the selectItemName function when the component mounts
+  React.useEffect(() => {
+    selectItemName();
+  }, []);
+
+  // Render the selected item name
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex:1,alignContent:"center",justifyContent:"center"}}>
+      <Text>Selected Item Name: {selectedItemName}</Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
